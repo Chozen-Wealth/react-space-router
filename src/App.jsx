b8234eff13c47d9fb6aec9c240a40a919447ed3b
chozen <moustafa.eldehni@hotmail.com>
2025-05-27 10:01:32 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Destination from './pages/Destination'
+import Crew from './pages/Crew'
+import Technology from './pages/Technology'
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/destination' element={<Destination/>} />
+        <Route path='/crew' element={<Crew/>} />
+        <Route path='/technology' element={<Technology/>} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
